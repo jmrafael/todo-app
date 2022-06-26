a3046ef455d2849bc95362c23279f41ec9cee219
Júlio Rafael <33831855+jmrafael@users.noreply.github.com>
2022-06-26 23:35:37 +0200

    Update Help.vue

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -2,9 +2,7 @@
   <q-page class="q-pa-lg">
     <h5 class="q-mt-none">Help</h5>
     <p style="font-size:1.2em">
-      This Todo Application is open source was created using VueJS and quasar <br> <br>
-      For Github Repo: <a href="https://github.com/created-by-varun"> Click here!</a> <br>
-      Created by <a href="https://its-varun.netlify.app">Varun</a>
+      This Todo Application is open source was created using VueJS and quasar
     </p>
 
   </q-page>
